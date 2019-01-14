b1b03652d0b76afe202e21b419ba9c6d4b2783be
Scripter17 <jameschristopherwise@gmail.com>
2019-01-14 12:05:59 -0500

    Made DIOBot actually start
    Why is it always the simple things that trip me up?
    (Probably because I tend to quit when something's too hard. :/)

--- Start.py
+++ Start.py
@@ -1,4 +1,4 @@
 import subprocess as sp
-commands=["python ReroBot.py", "python CancerBot.py", "python YesNo.py"]
+commands=["python ReroBot.py", "python CancerBot.py", "python YesNo.py", "python DIOBot.py"]
 proc=[sp.Popen(cmd, shell=True) for cmd in commands]
 for p in proc: p.wait()
