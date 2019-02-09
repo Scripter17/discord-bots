26f69fc3141046fec44baea3bf33eabbfb3dad1f
Scripter17 <jameschristopherwise@gmail.com>
2019-02-09 18:53:21 -0500

    Yeah, fuck it, MEMEBot was a failure.
    :sight_frown:

--- Start.py
+++ Start.py
@@ -3,8 +3,8 @@ commands=[
 	"cd reroBot && python reroBot.py",
 	"cd cancerBot && python cancerBot.py",
 	"cd ynBot && python ynBot.py",
-	"cd DIOBot && python DIOBot.py",
-	"cd memeBot && python memeBot.py"
+	"cd DIOBot && python DIOBot.py"
+	#"cd memeBot && python memeBot.py"
 ]
 proc=[sp.Popen(cmd, shell=True) for cmd in commands]
 for p in proc: p.wait()
