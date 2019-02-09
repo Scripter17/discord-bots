bab99e0b9762c3a16603a3fabc1ac163213b5504
Scripter17 <jameschristopherwise@gmail.com>
2019-02-09 11:11:08 -0500

    Round 3: A good ol' fashioned typo

--- cancerBot/cancerBot.py
+++ cancerBot/cancerBot.py
@@ -15,8 +15,8 @@ class __:
 	myServer=client.get_server(os.environ["myServer"])
 	class roles:
 		levels={
-			1: filter(lambda x:x!=None, [(x if x.name=="Diagnosed (level 1)" else None) for x in meServer.roles])[0],
-			4: filter(lambda x:x!=None, [(x if x.name=="Terminal (level 4)" else None) for x in meServer.roles])[0]
+			1: filter(lambda x:x!=None, [(x if x.name=="Diagnosed (level 1)" else None) for x in myServer.roles])[0],
+			4: filter(lambda x:x!=None, [(x if x.name=="Terminal (level 4)" else None) for x in myServer.roles])[0]
 		}
 
 class functions:
