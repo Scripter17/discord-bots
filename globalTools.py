dbfaaa9d5c861dc82dc3983c70fd422aaf412927
Scripter17 <jameschristopherwise@gmail.com>
2019-02-09 19:13:17 -0500

    Maybe I can get rid of the time prefix thing when viewing heroku logs via python...
    :thonk:

--- globalTools.py
+++ globalTools.py
@@ -1,6 +1,6 @@
 import time
 def log(msg, fmt="%Y-%m-%d %H:%M:%S (GMT%z)"):
-	print(time.strftime(fmt)+" "+msg)
+	print("\033[2K\033[48D"+time.strftime(fmt)+" "+msg)
 
 def getFunc(prefix, content):
 	return content[len(prefix):content.index(" ") if " " in content else None]
