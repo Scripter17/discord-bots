c590fcb864a85a79ef8921df8ee16a0c378825e0
Scripter17 <jameschristopherwise@gmail.com>
2019-06-20 20:54:00 -0400

    sgvdrfgt

--- Senko-bot/main.py
+++ Senko-bot/main.py
@@ -53,7 +53,7 @@ async def rainbowRole():
 
 @client.event
 async def on_ready():
-	global thanatos, notSoBot
+	global thanatos, notSoBot, theRealSenko, jolyneIrl, certifiedSenko
 	thanatos=await client.get_user_info("185220964810883072")
 	notSoBot=await client.get_user_info("439205512425504771")
 	theRealSenko=await client.get_user_info("335554170222542851")
