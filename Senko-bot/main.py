c6c8770d341519b8d726954153932bc8f896fcef
Scripter17 <jameschristopherwise@gmail.com>
2019-06-21 22:48:25 -0400

    I'm actually retarded

--- Senko-bot/main.py
+++ Senko-bot/main.py
@@ -44,8 +44,7 @@ async def on_message(message):
 			for mem in message.mentions:
 				await client.remove_roles(mem, certifiedSenko)
 		elif message.content.lower().startswith("$timer "):
-			print(message.content.split(" ")[1])
-			colorTime=parseInt(message.content.split(" ")[1])
+			colorTime=int(message.content.split(" ")[1])
 async def rainbowRole():
 	color=0
 	colors=[discord.Colour.red(), discord.Colour.orange(), discord.Colour.gold(), discord.Colour.green(), discord.Colour.blue(), discord.Colour.purple()]
