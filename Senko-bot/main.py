20f76793236ac0c2625a85260884adcbf6c21b6b
Scripter17 <jameschristopherwise@gmail.com>
2019-08-16 15:52:02 -0400

    Update main.py

--- Senko-bot/main.py
+++ Senko-bot/main.py
@@ -5,12 +5,16 @@ from discord.ext import commands
 
 bot=commands.Bot(command_prefix="$", help_command=None)
 
-owner=bot.get_user(335554170222542851)
-nsb=bot.get_user(439205512425504771)
-jolyne_irl=bot.get_guild(560507261341007902)
+owner=None
+nsb=None
+jolyne_irl=None
 
 @bot.event
 async def on_ready():
+	global owner, nsb, jolyne_irl
+	owner=bot.get_user(335554170222542851)
+	nsb=bot.get_user(439205512425504771)
+	jolyne_irl=bot.get_guild(560507261341007902)
 	bot.colours=[
 		discord.Colour.red(),
 		discord.Colour.orange(),
@@ -36,12 +40,14 @@ async def doRoles():
 @bot.command(name="verify", help="Owner only - Give user Certified Senko")
 async def verify(ctx,):
 	message=ctx.message
+	print("$verify called by "+str(ctx.message.author.id))
 	if message.author!=owner: return
 	role=discord.utils.get(message.guild.roles, name="Certified Senko")
 	[await m.add_roles(role, reason="Verified") for m in message.mentions]
 
 @bot.command(name="revoke", help="Owner only - Revoke person's Certified Senko")
 async def revoke(ctx):
+	print("$revoke called by "+str(ctx.message.author.id))
 	message=ctx.message
 	if message.author!=owner: return
 	role=discord.utils.get(message.guild.roles, name="Certified Senko")
@@ -49,6 +55,7 @@ async def revoke(ctx):
 
 @bot.command(name="list", help="List all Certified Senkos")
 async def list(ctx):
+	print("$list called by "+str(ctx.message.author.id))
 	message=ctx.message
 	role=discord.utils.get(message.guild.roles, name="Certified Senko")
 	await ctx.send("```"+"\n".join([x.name+"#"+x.discriminator for x in role.members])+"```")
@@ -62,6 +69,7 @@ Type `$ears` to get some transparent pictures of said ears.""")
 
 @bot.command(name="ears", help="Some ears to add to your pfp")
 async def ears(ctx):
+	print("$ears called by "+str(ctx.message.author.id))
 	await ctx.send(files=[
 		discord.File(open("Ear1.png", "rb")),
 		discord.File(open("Ear2.png", "rb"))
@@ -74,26 +82,36 @@ delChannel=set()
 async def on_message(message):
 	if message.author==bot.user:
 		return
+	#print(message.guild, jolyne_irl)
 	if message.guild==jolyne_irl:
 		if message.content.lower().split(" ")[0] in [".e621", ".r34", ".paheal", ".xbooru", ".yandera", ".pornhub"]:
+			print("Porn command detected: "+message.content+" (<@!"+str(message.author.id)+">)")
 			delFlag=False
 			tags=set(message.content.lower().split(" ")[1:])
+			# &tags=gardevoir won't trigger the Pokémon tag ban,
+			# but it will trigger the URL character ban
 			reply="Your command has been deleted for the following reasons:"
 			if tags&pokemonTags!=set():
+				#print("Pokémon tag detected")
 				delFlag=True
 				delChannel.add(message.channel)
 				reply+="\n- Pokémon tags ("+" ".join([str(x) for x in tags&pokemonTags])+")"
 			if set(urlChars)&set(message.content)!=set():
+				#print("URL char detected")
 				delFlag=True
 				delChannel.add(message.channel)
 				reply+="\n- URL escape characters"
-			reply+="\nThe command was:```"+message.content.replace("`", "`\u200b")+"```"
+			reply+="\nThe command was:```"+message.content.replace("`", "`\u200b")+"```" # \u200b = Zero-width space
+			reply+="\nNote: There is a slim chance that I deleted the wrong response from NotSoBot, if I did, then sorry."
 			if delFlag:
+				#print("Deleted command")
 				await message.channel.send(reply)
 				await message.delete()
 		elif message.channel in delChannel and message.author==nsb:
+			# TODO: Make this not accidentally delete the wrong result
 			delChannel.remove(message.channel)
 			await message.delete()
+			#print("Deleted NSB response")
 	await bot.process_commands(message)
 
 bot.run(os.environ["senkobottoken"])
