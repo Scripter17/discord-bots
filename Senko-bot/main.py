84e7b6650d169ee332aa2ff8d63ab143bf375456
Scripter17 <jameschristopherwise@gmail.com>
2019-11-17 13:12:14 -0500

    Too many memes

--- Senko-bot/main.py
+++ Senko-bot/main.py
@@ -1,5 +1,5 @@
 # https://discordapp.com/oauth2/authorize?client_id=578753573140299787&scope=bot
-import os, asyncio, sys
+import os, asyncio, sys, random
 sys.path.insert(0, "deps")
 import discord
 from discord.ext import commands
@@ -133,7 +133,7 @@ async def on_message(message):
 	# 623584630309650446 is meme-room in irene_irl
 	if message.author==owner and message.channel.id==623584630309650446 and (len(message.attachments)>=1 or isLink):
 		#await message.channel.send("Where's the funny", files=[discord.File(open("NotFunny/"+file, "rb")) for file in os.listdir("NotFunny")])
-		await message.channel.send("Where's the funny", file=discord.File(open("NotFunny.gif", "rb")))
+		await message.channel.send("Where's the funny", file=discord.File(open("NotFunny/"+random.choice(os.listdir("NotFunny")), "rb")))
 	await bot.process_commands(message)
 
 bot.run(os.environ["senkobottoken"])
