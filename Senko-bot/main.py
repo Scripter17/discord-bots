ea7eb540f32fa6348a51c13566c562319f915f7e
Scripter17 <jameschristopherwise@gmail.com>
2019-11-16 20:21:21 -0500

    :point_right::sung:sunglasses:lasses::point_right:

--- Senko-bot/main.py
+++ Senko-bot/main.py
@@ -133,7 +133,7 @@ async def on_message(message):
 	# 623584630309650446 is meme-room in irene_irl
 	if message.author==owner and message.channel.id==623584630309650446 and (len(message.attachments)>=1 or isLink):
 		#await message.channel.send("Where's the funny", files=[discord.File(open("NotFunny/"+file, "rb")) for file in os.listdir("NotFunny")])
-		await message.channel.send("Where's the funny", file=discord.File(open("NotFunny.png", "rb")))
+		await message.channel.send("Where's the funny", file=discord.File(open("NotFunny.gif", "rb")))
 	await bot.process_commands(message)
 
 bot.run(os.environ["senkobottoken"])
