e9a62498579db2d78bc50eaddd24f53cd152a7e1
Scripter17 <jameschristopherwise@gmail.com>
2019-12-30 12:19:50 -0500

    Wow I'm stupid

--- Senko-bot/main.py
+++ Senko-bot/main.py
@@ -20,9 +20,7 @@ async def on_ready():
 	owner=bot.get_user(335554170222542851)
 	nsb=bot.get_user(439205512425504771)
 	irene_irl=bot.get_guild(623576218595360778)
-	for member in irenr_irl.members:
-		if member.id==owner.id:
-			irene_owner=member
+	irene_owner=irene_irl.fetch_member(335554170222542851)
 	spluptoes=await irene_irl.fetch_member(342777816498176001)
 	bot.colours=[
 		discord.Colour.red(),
