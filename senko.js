5ce16ffca68c46706e919c5b059b7fad7f0bfd6a
Scripter17 <jameschristopherwise@gmail.com>
2020-01-25 17:07:26 -0500

    Does it seriously need to be strings?

--- senko.js
+++ senko.js
@@ -2,9 +2,9 @@ Discord=require("discord.js");
 bot=new Discord.Client();
 bot.on("ready",()=>{
 	data={
-		"owner":bot.users.get(335554170222542851),
-		"nsb":bot.users.get(439205512425504771),
-		"irene":bot.guilds.get(623576218595360778)
+		"owner":bot.users.get("335554170222542851"),
+		"nsb":bot.users.get("439205512425504771"),
+		"irene":bot.guilds.get("623576218595360778")
 	}
 	console.log(data);
 })
