7b3b0a88175134cea2e0c83669b8e468f4b0e1f9
Scripter17 <jameschristopherwise@gmail.com>
2020-01-26 22:35:08 -0500

    Added a not funny image and cleaned some of senko-bot's code
    This shouldn't break anything...

--- senko.js
+++ senko.js
@@ -19,35 +19,29 @@ bot.on("ready",()=>{
 		"pornCommands":[".e621", ".r34", ".paheal", ".xbooru", ".yandera", ".pornhub"],
 		"deleteChannels":[],
 		"exts":[".png", ".gif", ".jpg", ".jpeg", ".mp4", ".mov", ".bmp"],
-		"notFunny":fs.readdirSync("NotFunny")
+		"notFunny":fs.readdirSync("NotFunny").map(x=>"NotFunny/"+x)
 	};
-	data.memeChannel=data.irene.channels.find(x=>x.id=="623584630309650446")
+	data.memeChannel=data.irene.channels.find(x=>x.id=="623584630309650446");
 	setInterval(doRoles, 1000*15);
-	bot.on("message", function(m){
-		if (m.author.id==bot.user.id){return}
-		if (data.pornCommands.indexOf(m.content.toLowerCase().split(" ")[0])!=-1 && /[+%&#]/.test(m.content)){
-			data.deleteChannels.push(m.channel.id);
-			m.channel.send("Your command was deleted for using URL escape characters```"+m.content.replace(/`/g, "`\u200b")+"```There is a chance I deleted the wrong response by NSB, in which case I'm sorry");
-			m.delete();
-		} else if (m.author.id==data.nsb.id && data.deleteChannels.indexOf(m.channel.id)!=-1){
-			m.delete();
-			data.deleteChannels.splice(data.deleteChannels.indexOf(m.channel.id), 1);
-		} else if (m.author.id==data.owner.id
-			&& m.channel.id==data.memeChannel.id
-			&& m.attachments.array().length!=0){
-			console.log("Where's the funny");
-			m.channel.send("Where's the funny?", {"files":[
-					"NotFunny/"+data.notFunny[Math.floor(Math.random()*data.notFunny.length)]
-				]});
-		}
-	})
+	bot.on("message", onMessage);
 });
+function onMessage(m){
+	if (m.author.id==bot.user.id){return}
+	if (data.pornCommands.indexOf(m.content.toLowerCase().split(" ")[0])!=-1 && /[+%&#]/.test(m.content)){
+		data.deleteChannels.push(m.channel.id);
+		m.channel.send("Your command was deleted for using URL escape characters```"+m.content.replace(/`/g, "`\u200b")+"```There is a chance I deleted the wrong response by NSB, in which case I'm sorry");
+		m.delete();
+	} else if (m.author.id==data.nsb.id && data.deleteChannels.indexOf(m.channel.id)!=-1){
+		m.delete();
+		data.deleteChannels.splice(data.deleteChannels.indexOf(m.channel.id), 1);
+	} else if (m.author.id==data.owner.id && m.channel.id==data.memeChannel.id && m.attachments.array().length!=0){
+		m.channel.send("Where's the funny?", {"files":[data.notFunny[Math.floor(Math.random()*data.notFunny.length)]]});
+	}
+}
 function doRoles(){
 	for (guild of bot.guilds.array()){
-		try{
-			role=guild.roles.find(x=>x.name=="Certified Senko");
-			role.setColor(data.colors[data.ci]);
-		} catch (e) {}
+		role=guild.roles.find(x=>x.name=="Certified Senko");
+		role.setColor(data.colors[data.ci]);
 	}
 	data.ci=(data.ci+1)%data.colors.length;
 }
