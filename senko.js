7e34c1cc0ae4a7512830eb82e00edc44922c3acf
Scripter17 <jameschristopherwise@gmail.com>
2020-01-28 13:07:59 -0500

    Let me be clear: Enzo permitted me to do the "shut up pedo" memes.
    If he tells me to stop, I will stop

--- senko.js
+++ senko.js
@@ -15,18 +15,28 @@ bot.on("ready",()=>{
 			"#3498DB",
 			"#9B59B6"
 		],
+		"redditors":[
+			"497730525528850444" // YeeYee
+			"342777816498176001" // Spluptoes
+			"335554170222542851" // Me
+		],
+		"badMen":[
+			"215075528070266890" // Enzo-sama
+		],
+		"lastNotFunny":0,
 		"ci":0,
 		"pornCommands":[".e621", ".r34", ".paheal", ".xbooru", ".yandera", ".pornhub"],
 		"deleteChannels":[],
 		"exts":[".png", ".gif", ".jpg", ".jpeg", ".mp4", ".mov", ".bmp"],
-		"notFunny":fs.readdirSync("NotFunny").map(x=>"NotFunny/"+x)
+		"notFunny":fs.readdirSync("NotFunny").map(x=>"NotFunny/"+x),
+		"badMenIMG":fs.readdirSync("badMenIMG").map(x=>"badMenIMG/"+x)
 	};
 	data.memeChannel=data.irene.channels.find(x=>x.id=="623584630309650446");
 	setInterval(doRoles, 1000*15);
 	bot.on("message", onMessage);
 });
 function onMessage(m){
-	if (m.author.id==bot.user.id){return}
+	if (m.author.id==bot.user.id){return;}
 	if (data.pornCommands.indexOf(m.content.toLowerCase().split(" ")[0])!=-1 && /[+%&#]/.test(m.content)){
 		data.deleteChannels.push(m.channel.id);
 		m.channel.send("Your command was deleted for using URL escape characters```"+m.content.replace(/`/g, "`\u200b")+"```There is a chance I deleted the wrong response by NSB, in which case I'm sorry");
@@ -34,8 +44,14 @@ function onMessage(m){
 	} else if (m.author.id==data.nsb.id && data.deleteChannels.indexOf(m.channel.id)!=-1){
 		m.delete();
 		data.deleteChannels.splice(data.deleteChannels.indexOf(m.channel.id), 1);
-	} else if (m.author.id==data.owner.id && m.channel.id==data.memeChannel.id && m.attachments.array().length!=0){
-		m.channel.send("Where's the funny?", {"files":[data.notFunny[Math.floor(Math.random()*data.notFunny.length)]]});
+	} else if (new Date().getTime()-data.lastNotFunny>1000*60*5 && m.channel.id==data.memeChannel.id && (m.attachments.array().length!=0 || /\.(pnga?|jpe?g|gif|mp[34]|webm)$/.test(m.content.toLowerCase()))){
+		if (data.redditors.indexOf(m.author.id)!=-1){
+			m.channel.send("Where's the funny?", {"files":[data.notFunny[Math.floor(Math.random()*data.notFunny.length)]]});
+			data.lastNotFunny=new Date().getTime()
+		} else if (data.badMen.indexOf(m.author.id)!=-1){
+			m.channel.send("", {"files":[data.badMenIMG[Math.floor(Math.random()*data.badMenIMG.length)]]});
+			data.lastNotFunny=new Date().getTime()
+		}
 	}
 }
 function doRoles(){
