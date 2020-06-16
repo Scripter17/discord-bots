a3576c95a2a944099421e3cbcb629f839def1764
Scripter17 <jameschristopherwise@gmail.com>
2020-06-16 12:56:12 -0400

    Update senko.js

--- senko.js
+++ senko.js
@@ -53,12 +53,6 @@ bot.on("ready",()=>{
 			"333628523795447808"  // Teffy
 		]
 	};
-	/*data.SSTJailData={
-		"role":data.SST.roles.find(x=>x.id=="692146778409009162"),
-		"senkoID":"691882177691910155",
-		"senkletID":"691882248991146037",
-		"logChannel":data.SST.channels.find(x=>x.id=="700299921177313381")
-	}*/
 	data.serverRolePeriod=deltaNotationArray(lcm, [].concat(...Object.values(data.serverRoles).map(Object.values)).map(x=>x.length));//Object.values(data.daiyaRoles).map(x=>x.length));
 	console.log("Senko bot booted")
 	//console.log(data)
@@ -94,24 +88,12 @@ function onMessage(m){
 		rfiles;
 	member=m.guild.members.get(m.author.id);
 	
-	if (m.guild==data.SST && m.author==data.owner){
+	if (m.guild==data.SST && m.author==data.owner && m.content!="" && ["691884413558194186", "692926620822143028", "713295801203425322", "712514087480983574", "712395282935185419"].indexOf(m.channel.id)==-1){
 		m.delete();
-		data.owner.send("Don't even bother");
+		data.owner.send("Bitch");
 		return;
 	}
-	/*if (m.guild==data.SST && m.mentions.members.array().indexOf(data.SST.owner)!=-1){
-		if (new Date().getTime()-data.lastNotFunny>1000*60*5){
-			m.channel.send({"files":["haha funny pedo joke.png"]});
-			data.lastNotFunny=new Date().getTime();
-		}
-		m.delete();
-	}*/
-	
-	/*isSSTMod=member._roles.indexOf(data.SSTJailData.senkoID)!=-1 || member._roles.indexOf(data.SSTJailData.senkletID)!=-1;
-	if (m.content.toLowerCase().startsWith("$jail")){
-		//console.log(m.mentions.users)
-		doJailStuff(m);
-	}*/
+
 	if (data.pornCommands.indexOf(m.content.toLowerCase().split(" ")[0])!=-1 && /[+%&#]/.test(m.content)){
 		data.deleteChannels.push(m.channel.id);
 		m.channel.send("Your command was deleted for using URL escape characters```"+m.content.replace(/`/g, "`\u200b")+"```There is a chance I deleted the wrong response by NSB, in which case I'm sorry");
@@ -136,41 +118,6 @@ function onMessage(m){
 	}
 }
 
-/*function doJailStuff(message){
-	var mentions, warnUser, timeout, jailData;
-	mentions=message.mentions.members;
-	data.SSTJailData.jailBusy=true;
-	data.SSTJailData.logChannel.fetchMessages().then(console.log)
-	data.SSTJailData.logChannel.fetchMessages({"limit":1}).then(function(jailData){
-		console.log(jailData)
-		jailData=JSON.parse("{"+jailData.first().content.replace(/```(JSON\n)?/g, "").replace(/(\d+)(?=:)/g, '"$1"').split("\n").join(",")+"}");//.split("\n").map(x=>x.split(" "))
-		mentions.forEach(function(mention){
-			warnUser=Object.keys(jailData).indexOf(mention.id)==-1 || new Date().getTime()-jailData[mention.id].last>=1000*60*60*24*7;
-			if (Object.keys(jailData).indexOf(mention.id)==-1){
-				// Initialize jail data
-				jailData[mention.id]={"name":mention.user.username, "time":5, "last":new Date().getTime()};
-			} else {
-				jailData[mention.id].name=mention.user.username; // People have been jailed before I added this
-				jailData[mention.id].last=new Date().getTime();
-			}
-			console.log("Jail for <@"+mention.id+"> ("+mention.user.username+")");
-			if (warnUser){
-				message.channel.send("Warned "+mention+". Next $jail will last "+jailData[mention.id].time+" minutes")
-			} else {
-				timeout=jailData[mention.id].time*1000*60;
-				mention.addRole(data.SSTJailData.role);
-				message.channel.send("Jailed "+mention+" for "+jailData[mention.id].time+" minutes");
-				setTimeout(function(){
-					mention.removeRole(data.SSTJailData.role);
-				}, timeout);
-				jailData[mention.id].time+=5;
-			}
-		})
-		data.SSTJailData.logChannel.send("```JSON\n"+Object.keys(jailData).map(x=>x+":"+JSON.stringify(jailData[x])).join("\n")+"```");
-		data.SSTJailData.jailBusy=false;
-	})
-}*/
-
 function doRoles(){
 	var role, roleId;
 	for (guild of bot.guilds.array()){
@@ -180,12 +127,6 @@ function doRoles(){
 				role.setColor(data.colors[data.ci]);
 			} catch (e) {console.log("Failed setting senko role color for ", guild.name)}
 		}
-		/*if (guild.id=="647203852990414889"){
-			for (id in data.daiyaRoles){
-				guild.roles.find(x=>x.id==id).setColor(data.daiyaRoles[id][data.daiyaIndex%data.daiyaRoles[id].length]).catch(console.log)
-			}
-			data.daiyaIndex=(data.daiyaIndex+1)%data.daiyaPeriod;
-		}/**/
 		if (guild.id in data.serverRoles){
 			for (roleId in data.serverRoles[guild.id]){
 				role=guild.roles.find(x=>x.id==roleId);
