8d5c30d1e75428ab46d588d93bf4d32553bd6c62
Scripter17 <jameschristopherwise@gmail.com>
2021-08-24 13:17:23 -0400

    Make dualbot work

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-worker: node senko.js
-worker: python yadb.py
+worker: python main.py
---
+++ main.py
@@ -0,0 +1,8 @@
+import subprocess as sp
+commands=[
+	"node senko.js",
+	"python yadb.py"
+]
+procs=[sp.Popen(command, shell=True) for command in commands]\
+for proc in procs:
+	proc.wait()
