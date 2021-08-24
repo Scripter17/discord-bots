54d6ee3bb26706dc5e4058f77f56ba93ab63f3d9
Scripter17 <jameschristopherwise@gmail.com>
2021-08-24 13:18:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ commands=[
 	"node senko.js",
 	"python yadb.py"
 ]
-procs=[sp.Popen(command, shell=True) for command in commands]\
+procs=[sp.Popen(command, shell=True) for command in commands]
 for proc in procs:
 	proc.wait()
