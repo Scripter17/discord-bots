72003deeff33183a3e396ccfdbbc96dc747e8703
Scripter17 <jameschristopherwise@gmail.com>
2021-09-12 19:39:31 -0400

    [YADB] Add ?advRoll

--- yadb.py
+++ yadb.py
@@ -28,6 +28,39 @@ def rollDice(dice):
 			rolls.append(int(die))
 	return " + ".join([str(x) for x in rolls])+" = "+str(sum(rolls))
 
+def advancedRollDice(diceString):
+	reCount= r"((?<!\))\d*)"
+	reMin  = r"(?:(\d+)\.\.)"
+	reSides= r"((?:\d+,)*)"
+	reMode =fr"(?:{reMin}|{reSides})?"
+	reSize = r"(\d+)"
+	reKeep = r"(kl?)?"
+	reDice =fr"{reCount}d{reMode}{reSides}{reKeep}"
+	def _rollDice(diceString):
+		count, minimum, sides, size, keep=re.findall(reDice, diceString[0])[0]
+		count  =int(count or "1")
+		minimum=int(minimum[:-1] or "1")
+		size   =int(size)
+		if sides:
+			sides=[size, *[int(x) for x in sides.split(",") if x]]
+		ret=[]
+		if count>65535:
+			return "Sorry, I'm not running this on an RTXXX 69420"
+		for i in range(count):
+			if sides:
+				ret.append(random.choice(sides))
+			else:
+				ret.append(random.randint(minimum, size))
+		return str({"k":max, "kl":min, "":sum}[keep](ret))
+	diceString=re.sub(reDice, _rollDice, diceString.lower())
+	if re.search(r"\(\d+\)", diceString):
+		diceString=advancedRollDice(re.sub(r"\((\d+)\)", "\\1", diceString))
+	if re.search(r"[^\d*+\-/%()\.]", diceString):
+		raise SyntaxError("Possible ACE detected: "+diceString)
+	else:
+		diceString=str(eval(diceString))
+	return diceString
+
 r""" CLEAN VERSION
 def rollDice(dice):
 	dice=re.findall(r"([+-]?(?:\d*d)?\d+)", dice.lower())
@@ -50,6 +83,20 @@ async def cmdDice(ctx):
 		await ctx.channel.send(rollDice(ctx.message.content), reference=ctx.message) # Yeah I know this is bad. Sue me (Please don't)
 	except discord.errors.HTTPException:
 		await ctx.channel.send("Pisscorp has a finite message length limit :/", reference=ctx.message)
+	except Exception as e:
+		print(e)
+		await ctx.channel.send("Unknown error occured. Go yell at Senko", reference=ctx.message)
+
+@bot.command(aliases=["advroll", "advdice", "advr", "advancedroll", "advancedice", "advancedr", "roll2", "dice2", "r2"])
+async def cmdAdvDice(ctx, diceString):
+	try:
+		await ctx.channel.send(advancedRollDice(diceString), reference=ctx.message)
+	except discord.errors.HTTPException:
+		await ctx.channel.send("Pisscorp has a finite message length limit :/", reference=ctx.message)
+	except Exception as e:
+		print(e)
+		await ctx.channel.send("Unknown error occured. Go yell at Senko", reference=ctx.message)
+
 @bot.command(aliases=["help", "what", "wat", "wot"])
 async def cmdHelp(ctx):
 	if random.randint(1,100)==69:
@@ -63,10 +110,18 @@ async def cmdHelp(ctx):
 			startLine+"""
 				I was made by Senko, that one guy I'm pretty sure you all hate (I think. I don't actually know who's gonna be using this)
 				Here's how to roll dice:
-					`?roll d6` = Roll a d6
-					`?roll 4d6` = Roll 4 d6's
-					`?roll 2d6+1d4` = Roll 2 d6's and then a d4
+					`?roll d6`        = Roll a d6
+					`?roll 4d6`       = Roll 4 d6's
+					`?roll 2d6+1d4`   = Roll 2 d6's and then a d4
 					`?roll 2d6+1d4-8` = Roll 2 d6's, add a d4, then subtract 8
+				Using the state of the art ?advRoll command, you can do the following:
+					`?r2 (2d4)d4`  = Roll 2d4 then roll that many d4's
+					`?r2 (2d4**2)` = Roll a 2d4 then square it
+					`?r2 4d20k`    = Roll 4 d20's and keep the highest
+					`?r2 4d20kl`   = Above bu7t keep the lowest
+					`?r2 d1,20`    = Roll a 1 or a 20
+					`?r2 d10..20`  = Basically just `?r2 d10+10`
+					You can use the +, -, /, %, //, *, and ** python operators as well as parenthesis
 				To use `?choose` you pass it some options like a command line
 				You can unfortunately use `?ask` to ask me questions
 
@@ -106,7 +161,7 @@ async def cmdAsk(ctx):
 async def cmdChoose(ctx, *args):
 	await ctx.channel.send(random.choice(args), reference=ctx.message)
 
-@bot.command(aliases=["nomran", "IFuckingHateSpluptoAndIHopeHeKnowsThis", "norman"])
+@bot.command(aliases=["nomran", "norman"])
 async def cmdNorman(ctx):
 	await ctx.channel.send("norman of the north", file=discord.File(open("norman.jpeg", "rb")))
 
