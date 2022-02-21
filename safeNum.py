35781ffa1b3e85ce2ace7312fac40e7aca23c57c
Scripter17 <jameschristopherwise@gmail.com>
2022-02-21 05:24:42 -0500

    [yadb] Hopefully fix all the TypeErrors caused by SafeNum

--- safeNum.py
+++ safeNum.py
@@ -12,17 +12,32 @@ class SafeNum:
 	def __truediv__ (self, other): return SafeNum(self.num/ SafeNum(other).num)
 	def __floordiv__(self, other): return SafeNum(self.num//SafeNum(other).num)
 	def __mod__     (self, other): return SafeNum(self.num% SafeNum(other).num)
+	
+	def __radd__     (self, other): return self+ other
+	def __rsub__     (self, other): return self- other
+	def __rmul__     (self, other): return self* other
+	def __rmatmul__  (self, other): return self@ other
+	def __rtruediv__ (self, other): return self/ other
+	def __rfloordiv__(self, other): return self//other
+	def __rmod__     (self, other): return self% other
 
-	def __pow__(self, other):
+	def __pow__(self, other, mod=None):
 		if self*other>=1000:
 			raise ValueError(f"An exponentiation that's too big was detected ({self}**{other})")
-		return SafeNum(self.num**SafeNum(other).num)
+		return SafeNum(pow(self.num, SafeNum(other).num, mod))
+	def __rpow__(self, other, mod=None): return pow(self, other, mod)
 
 	def __lshift__(self, other): return SafeNum(self.num<<SafeNum(other).num)
 	def __rshift__(self, other): return SafeNum(self.num>>SafeNum(other).num)
 	def __and__   (self, other): return SafeNum(self.num& SafeNum(other).num)
 	def __xor__   (self, other): return SafeNum(self.num^ SafeNum(other).num)
 	def __or__    (self, other): return SafeNum(self.num| SafeNum(other).num)
+
+	def __rlshift__(self, other): return self<<other
+	def __rrshift__(self, other): return self>>other
+	def __rand__   (self, other): return self& other
+	def __rxor__   (self, other): return self^ other
+	def __ror__    (self, other): return self| other
 
 	def __pos__   (self): return SafeNum(+self.num)
 	def __neg__   (self): return SafeNum(-self.num)
@@ -35,11 +50,10 @@ class SafeNum:
 	def __lt__(self, other): return SafeNum(self.num< SafeNum(other).num)
 	def __ne__(self, other): return SafeNum(self.num!=SafeNum(other).num)
 
-	def __bool__(self): return bool(self.num)
-
 	def __str__ (self): return str(self.num)
 	def __repr__(self): return str(self)
 
+	def __bool__(self): return bool(self.num)
 	def __int__    (self): return int(self.num)
 	def __float__  (self): return float(self.num)
 	def __complex__(self): return complex(self.num)
